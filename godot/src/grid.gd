a98233d0a0f1384c5e73ad441b388ec83bfd82fb
myin <minyin142p@gmail.com>
2023-07-31 19:03:36 +0200

    change to control nodes

--- godot/src/grid.gd
+++ godot/src/grid.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends GridContainer
 
 
 const pieces = [
@@ -9,18 +9,13 @@ const pieces = [
 	preload("res://src/piece/calli.tscn"),
 ]
 
-@export var width := 8
-@export var height := 8
+@export var rows := 8
 @export var piece_size := 128
 
-@onready var grid_size := Vector2(width, height)
+@onready var grid_size := Vector2(columns, rows)
 
 var offset = Vector2.ZERO
 var data: Array[Array] = []
-
-func _notification(what):
-	if what == NOTIFICATION_WM_SIZE_CHANGED:
-		pass
 
 func _ready():
 	offset = _get_grid_offset()
@@ -34,44 +29,40 @@ func _get_grid_offset():
 
 func _create_data() -> Array[Array]:
 	var arr: Array[Array] = []
-	for x in width:
+	for x in columns:
 		arr.append([])
-		for y in height:
+		for y in rows:
 			arr[x].append(null)
 	return arr
 
 func _spawn_pieces():
-	for x in width:
-		for y in height:
+	for y in rows:
+		for x in columns:
 			var piece = pieces.pick_random()
-			var node = null
 			var loops = 0
-			while node == null or get_match_count(x, y) > 0 and loops < 100:
+			while loops == 0 or get_match_count(x, y) > 0 and loops < 100:
 				piece = pieces.pick_random()
 				loops += 1
-				node = piece.instantiate()
-				data[x][y] = node
+				data[x][y] = piece.instantiate()
 			
-			add_child(node)
-			node.global_position = _grid_to_global(Vector2(x, y))
+			add_child(data[x][y])
 
 func get_match_count(x: int, y: int):
 	var piece = _get_type(x, y)
-	if x > 1:
-		var row = [piece, _get_type(x-1, y), _get_type(x-2, y)]
-		var count = row.count(piece)
-		if count >= 3:
-			return count
-	if y > 1:
-		var col = [piece, _get_type(x, y-1), _get_type(x, y-2)]
-		var count = col.count(piece)
-		if count >= 3:
-			return count
+	if piece:
+		if x > 1:
+			var row = [piece, _get_type(x-1, y), _get_type(x-2, y)]
+			var count = row.count(piece)
+			if count >= 3:
+				return count
+		if y > 1:
+			var col = [piece, _get_type(x, y-1), _get_type(x, y-2)]
+			var count = col.count(piece)
+			if count >= 3:
+				return count
+
 	return 0
 
 func _get_type(x: int, y: int):
 	var d = data[x][y]
 	return d.type if d else null
-
-func _grid_to_global(pos: Vector2):
-	return offset + pos * piece_size + Vector2(1, 1) * piece_size / 2 
