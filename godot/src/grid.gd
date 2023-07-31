445ed5f555b29e3cff195366dd6e437017f85894
myin <minyin142p@gmail.com>
2023-07-31 20:02:04 +0200

    swap pieces

--- godot/src/grid.gd
+++ godot/src/grid.gd
@@ -39,17 +39,38 @@ func _spawn_pieces():
 	for y in rows:
 		for x in columns:
 			var piece = pieces.pick_random()
+			var node: Piece
 			var loops = 0
-			while loops == 0 or get_match_count(x, y) > 0 and loops < 100:
+			while node == null or (get_match_count(x, y) > 0 and loops < 100):
 				piece = pieces.pick_random()
 				loops += 1
-				data[x][y] = piece.instantiate()
+				node = piece.instantiate()
+				data[x][y] = node
 			
-			add_child(data[x][y])
+			add_child(node)
+			var pos = Vector2(x, y)
+			node.swiped.connect(func(dir): _move_pieces(pos, dir))
+
+func _move_pieces(pos: Vector2, dir: Vector2):
+	var piece = data[pos.x][pos.y]
+	var dest = pos + dir
+	var other = data[dest.x][dest.y]
+	
+	print("Move ", pos, " - ", piece.type_name())
+	print("Dest ", dest, " - ", other.type_name())
+	
+	data[pos.x][pos.y] = other
+	data[dest.x][dest.y] = piece
+	
+	# TODO: actually move it inside the grid and disable input while moving
+	var piece_pos = piece.global_position
+	piece.move(other.global_position)
+	other.move(piece_pos)
+	
 
 func get_match_count(x: int, y: int):
 	var piece = _get_type(x, y)
-	if piece:
+	if piece != null:
 		if x > 1:
 			var row = [piece, _get_type(x-1, y), _get_type(x-2, y)]
 			var count = row.count(piece)
--- godot/src/piece/piece.gd
+++ godot/src/piece/piece.gd
@@ -1,4 +1,8 @@
+class_name Piece
 extends Control
+
+signal pressed
+signal swiped(dir)
 
 enum Type {
 	INA,
@@ -9,3 +13,32 @@ enum Type {
 }
 
 @export var type: Type
+@export var swipe_threshold := 100
+
+var swipe_start
+
+func _gui_input(event: InputEvent):
+	if event.is_action_pressed("click") and event is InputEventMouseButton:
+		pressed.emit()
+		swipe_start = event.global_position
+	if event.is_action_released("click") and event is InputEventMouseButton:
+		_calculate_swipe(event.global_position)
+		swipe_start = null
+
+func _calculate_swipe(swipe_end):
+	if swipe_start == null:
+		return
+	var swipe: Vector2 = swipe_end - swipe_start
+	
+	if swipe.length() > swipe_threshold:
+		var dir = swipe.normalized().round()
+		if dir.length() > 1:
+			dir.y = 0
+		print("Swipe for ", Type.keys()[type])
+		swiped.emit(dir)
+
+func move(dest: Vector2):
+	create_tween().tween_property(self, "global_position", dest, 0.5)#.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BOUNCE)
+
+func type_name():
+	return Type.keys()[type]
