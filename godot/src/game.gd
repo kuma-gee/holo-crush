825fd525eb9f88818edea4ef29a20b9be5e6d477
myin <minyin142p@gmail.com>
2023-08-05 14:21:29 +0200

    add gameover screen

--- godot/src/game.gd
+++ godot/src/game.gd
@@ -3,12 +3,15 @@ extends Control
 @export var input_blocker: Control
 
 @export var turns_label: Label
-@export var turns := 10
+@export var turns := 5
 
+@export var gameover: Control
+@export var end_score: Label
 @export var score_label: Label
 var score = 0
 
 func _ready():
+	gameover.hide()
 	input_blocker.hide()
 	turns_label.text = str(turns)
 	score_label.text = str(score)
@@ -21,15 +24,21 @@ func _on_grid_processing():
 func _on_grid_processing_finished():
 	if turns > 0:
 		input_blocker.hide()
+	else:
+		_on_gameover()
 
+func _on_gameover():
+	end_score.text = tr("Score") + ": " + str(score)
+	gameover.show()
 
 func _on_grid_swapped():
 	turns -= 1
 	turns_label.text = str(turns)
-	if turns <= 0:
-		pass
-
 
 func _on_grid_scored(value):
 	score += value
 	score_label.text = str(score)
+
+
+func _on_restart_pressed():
+	get_tree().reload_current_scene()
