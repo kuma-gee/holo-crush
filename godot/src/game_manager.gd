d7d2add4c6d3444b21022967c54ca09c1cb93494
myin <minyin142p@gmail.com>
2023-08-14 19:39:52 +0200

    feat: add energy ui

--- godot/src/game_manager.gd
+++ godot/src/game_manager.gd
@@ -1,16 +1,22 @@
 extends Node
 
+signal energy_updated(energy)
+
 var points := 0
 
 var max_energy := 5
-@onready var energy := max_energy
+@onready var energy := max_energy : set = _set_energy
+
+func _set_energy(v):
+	energy = v
+	energy_updated.emit(energy)
 
 func scored(score: int):
 	points += floor(score / 100)
 
 func start_game():
-#	if GameManager.energy > 0:
-#		energy -= 1
-	SceneManager.change_scene("res://src/game.tscn")
-#	else:
-#		print("No energy")
+	if energy > 0:
+		self.energy -= 1
+		SceneManager.change_scene("res://src/game.tscn")
+	else:
+		print("No energy")
--- godot/src/start.gd
+++ godot/src/start.gd
@@ -1,12 +1,11 @@
 extends Control
 
-@export var energy: Label
+@export var energy: EnergyContainer
 @export var points: Label
 
 @onready var anim := $AnimationPlayer
 
 func _ready():
-	energy.text = str(GameManager.energy)
 	points.text = str(GameManager.points)
 
 func _on_play_pressed():
