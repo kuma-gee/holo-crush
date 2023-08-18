ed4f70fa5d4a7c03c763c645948b5982bb2e24de
myin <minyin142p@gmail.com>
2023-08-18 19:54:41 +0200

    refacctor

--- godot/src/menu/store/GachaPack.gd
+++ godot/src/menu/store/GachaPack.gd
@@ -4,6 +4,11 @@ extends TextureButton
 @export var price := 0
 @export var pieces: Array[Piece.Type] = []
 
+@onready var price_label := $Price
+
+func _ready():
+	price_label.text = str(price)
+
 func get_pieces():
 	if pieces.size() > 0:
 		return pieces
--- godot/src/menu/store/Store.gd
+++ godot/src/menu/store/Store.gd
@@ -2,10 +2,15 @@ extends Control
 
 @export var pack_container: Control
 
+@onready var showcase := $Showcase
 @onready var store_container := $PanelContainer
+@onready var anim := $AnimationPlayer
+
+var showing_showcase = false
 
 func _ready():
 	store_container.hide()
+	showcase.hide()
 	
 	for child in pack_container.get_children():
 		child.pressed.connect(func(): _unpack_gacha(child))
@@ -13,6 +18,7 @@ func _ready():
 func _on_store_pressed():
 	if store_container.visible:
 		store_container.hide()
+		showcase.hide()
 	else:
 		store_container.show()
 
@@ -21,5 +27,17 @@ func _unpack_gacha(pack: GachaPack):
 		var new_piece = pack.get_pieces().pick_random()
 		GameManager.unlock_piece(new_piece)
 		print("unlocked %s" % Piece.Type.keys()[new_piece])
+		anim.play("showcase")
+		await anim.animation_finished
+		showing_showcase = true
 	else:
 		print("No money")
+
+func _on_showcase_on_hide():
+	if not showing_showcase:
+		return
+		
+	anim.play_backwards("showcase")
+	await anim.animation_finished
+	showcase.hide()
+	showing_showcase = false
